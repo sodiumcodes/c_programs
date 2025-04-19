47a258dca588231f8062e49727362bec8ec85ec2
nainascode <nainadugar.official@gmail.com>
2025-04-19 21:58:56 +0530

    Implement strcmp

---
+++ HackerRank/arrays_20.c
@@ -0,0 +1,104 @@
+//You are given two strings. Your task is to compare these two strings lexicographically using logic similar to the strcmp() function in C. The function compares the characters of the two strings one by one from left to right.
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the 'strcmp' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s1
+ *  2. STRING s2
+ */
+
+int strCmp(char* s1, char* s2) {
+    int i= strlen(s1);
+    int j= strlen(s2);
+    int x=0, y=0;
+    while(x<i && y<j){
+        int z=s1[x++]- s2[y++];
+        if(z){
+            return z;
+        }
+    }
+    return i-j;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s1 = readline();
+
+    char* s2 = readline();
+
+    int result = strCmp(s1, s2);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
